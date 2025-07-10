c56a6850a23acfcc03c1ffdab6c05ba89e91631c
Samuel Maenle <samuel@maenle.net>
2025-07-10 12:51:00 +0200

    Updated to work with new MediaWiki\Title\Title in 1.44

--- includes/Targets.php
+++ includes/Targets.php
@@ -24,6 +24,7 @@
  */
 namespace LinkTitles;
 
+use MediaWiki\Title\Title as MWTitle;
 use MediaWiki\MediaWikiServices;
 
 /**
@@ -43,7 +44,7 @@ class Targets {
 	 * @param  String $sourceNamespace The namespace of the current page.
 	 * @param  Config $config    LinkTitles configuration.
 	 */
-	public static function singleton( \Title $title, Config $config ) {
+	public static function singleton( MWTitle $title, Config $config ) {
 		if ( ( self::$instance === null ) || ( self::$instance->sourceNamespace != $title->getNamespace() ) ) {
 			self::$instance = new Targets( $title, $config );
 		}
@@ -85,9 +86,9 @@ class Targets {
 
 	/**
 	 * The constructor is private to enforce using the singleton pattern.
-	 * @param  \Title $title
+	 * @param MWTitle $title
 	 */
-	private function __construct( \Title $title, Config $config) {
+	private function __construct( MWTitle $title, Config $config) {
 		$this->config = $config;
 		$this->sourceNamespace = $title->getNamespace();
 		$this->fetch();
